186bc2ceca5e67cd7bb4cfc03737bec1519a6db9
Ibango7 <israel.bango@boxfusion.io>
2024-04-08 12:24:51 +0200

    backend initialisation

---
+++ Lms-backend/aspnet-core/src/LmsBackend.Application/Services/BookManagerService/Dto/BookManagementDto.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using LmsBackend.Authorization.Users;
+using LmsBackend.Entities;
+using LmsBackend.Services.BooksService.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LmsBackend.Services.BookManagerService.Dto 
+{
+    [AutoMap(typeof(BookManagement))]
+    public class BookManagementDto: EntityDto<Guid>
+    {
+        public virtual bool Returned { get; set; }
+        public virtual bool Overdue { get; set; }
+        public virtual DateTime DateBorrowed { get; set; }
+
+
+        public virtual BookDto Book { get; set; }
+
+        public virtual long UserId { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
---
+++ Lms-backend/aspnet-core/src/LmsBackend.Application/Services/BookManagerService/Dto/RentedBookDto.cs
@@ -0,0 +1,25 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LmsBackend.Services.BookManagerService.Dto
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RentedBookDto: EntityDto<Guid>
+    {
+
+        public long UserId { get; set; }
+        public Guid BookId { get; set; }
+        public DateTime DateBorrowed { get; set; }
+        public bool Returned { get; set; }
+        public bool Overdue { get; set; }
+        // Additional properties for book information
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+    }
+}
