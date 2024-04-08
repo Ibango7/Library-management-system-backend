0ca77a19980a32380dccd1dc3cdaabaceec0bcb5
Ibango7 <israel.bango@boxfusion.io>
2024-04-08 12:26:04 +0200

    backend-commit

---
+++ Lms-backend/aspnet-core/src/LmsBackend.Application/Configuration/ConfigurationAppService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Abp.Authorization;
+using Abp.Runtime.Session;
+using LmsBackend.Configuration.Dto;
+
+namespace LmsBackend.Configuration
+{
+    [AbpAuthorize]
+    public class ConfigurationAppService : LmsBackendAppServiceBase, IConfigurationAppService
+    {
+        public async Task ChangeUiTheme(ChangeUiThemeInput input)
+        {
+            await SettingManager.ChangeSettingForUserAsync(AbpSession.ToUserIdentifier(), AppSettingNames.UiTheme, input.Theme);
+        }
+    }
+}
---
+++ Lms-backend/aspnet-core/src/LmsBackend.Application/Configuration/IConfigurationAppService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using LmsBackend.Configuration.Dto;
+
+namespace LmsBackend.Configuration
+{
+    public interface IConfigurationAppService
+    {
+        Task ChangeUiTheme(ChangeUiThemeInput input);
+    }
+}
