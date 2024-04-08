1c8f556535b72d21800fa287dbe3a8be0f564ada
Ibango7 <israel.bango@boxfusion.io>
2024-04-08 12:26:38 +0200

    committed

---
+++ Lms-backend/aspnet-core/src/LmsBackend.Application/MultiTenancy/ITenantAppService.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Application.Services;
+using LmsBackend.MultiTenancy.Dto;
+
+namespace LmsBackend.MultiTenancy
+{
+    public interface ITenantAppService : IAsyncCrudAppService<TenantDto, int, PagedTenantResultRequestDto, CreateTenantDto, TenantDto>
+    {
+    }
+}
+
